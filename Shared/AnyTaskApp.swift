80a0b63cf4343a26d2fb03458d6f4fc483d1d148
Milad Golchinpour <miladsoftware@icloud.com>
2021-08-04 14:02:05 +0430

    Initial Commit

---
+++ Shared/AnyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyTaskApp.swift
+//  Shared
+//
+//  Created by Milad Golchinpour on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct AnyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
